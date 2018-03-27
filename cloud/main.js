449cadd61de9f5d2606b1bad40f8518a7661574b
Tigran Atayan <tigran.a.atayan@gmail.com>
2018-03-27 21:50:59 +0400

    Added function for getting items from home page,

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,74 @@
+Parse.Cloud.define("HomePage", function(request, response) {
+    let tableToObjectIds = {};
+    let fieldsToOverride = {};
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+    //  Getting all fields from Home class.
+    const query = new Parse.Query("Home");
+
+    //  Executing the query.
+    query.find()
+        .then((results) => {
+            let promises = [];
+
+            //  Preparing results.
+            results.forEach((item) => {
+                const refObjectId = item.get('refObjectId');
+                const info = refObjectId.split(':');
+
+                if (info.length != 2) {
+                    continue;
+                }
+
+                const table = info[0];
+                const objectId = info[1];
+
+                //  Creating container for items for a particular table.
+                let tableItems = tableToObjectIds[table] || (tableToObjectIds[table] = []);
+
+                //  Keeping custom information of Home class to override fields 
+                //  which come from different tables but are specified here as well.
+                const customFields = Object.assign({}, item.attributes);
+                delete customFields.refObjectId;
+
+                //  Saving the needed item id.
+                tableItems.push(objectId);
+                fieldsToOverride[refObjectId] = customFields;
+            });
+
+            //  Getting original items.
+            for (const table in tableToObjectIds) {
+                const objectIds = table2ObjectIds[table];
+                const query = new Parse.Query(table);
+
+                //  Getting only needed items from this table.
+                query.containedIn("objectId", objectIds);
+
+                //  Sending the request and saving the promise in promises array.
+                promises.push(query.find());
+            }
+
+            return Parse.Promise.all(promises);
+        })
+        .then((realItemsList) => {
+            let i = 0;
+
+            for (const table in tableToObjectIds) {
+                const items = realItemsList[i++];
+
+                items.forEach((item) => {
+                    const refObjectId = table + ':' + item.get('objectId');
+                    const fields = item.attributes;
+
+                    const toOverride = fieldsToOverride[refObjectId];
+                    if (toOverride) {
+                        Object.assign(item.attributes, toOverride);
+                    }
+                });
+            }
+
+            response.success(realItemsList);
+        })
+        .catch(() => {
+            response.error("Couldn't get information for home page.");
+        });
 });
